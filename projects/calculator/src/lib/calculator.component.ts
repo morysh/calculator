58d807ecb68919be7edc4e1bea4a24c8e34fdceb
Adrien CARLIER-MORY <adrien@carlier-mory.com>
2023-03-09 20:28:35 +0100

    initial commit

---
+++ projects/calculator/src/lib/calculator.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'glady-calculator',
+  templateUrl: './calculator.component.html',
+  styles: [],
+})
+export class CalculatorComponent {}
